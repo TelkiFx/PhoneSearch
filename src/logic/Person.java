821f62ace44bf9f256e23933d4e32d90e52ba0c1
Cameron <camtredoux@gmail.com>
2019-12-02 20:24:48 -0700

    Changed Person to require methods to be populated with data (vs. using a constructor)

--- src/logic/Person.java
+++ src/logic/Person.java
@@ -6,18 +6,24 @@ public class Person {
     private String phoneNumber;
     private String address;
 
-    public Person(String name, String phoneNumber, String address) {
+    public void setName(String name) {
         this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getAddress() {
